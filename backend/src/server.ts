6b6589d8e9c76d6b0db92d002a8e206e969cecc6
andrefangeloni <andrefangeloni@gmail.com>
2020-07-19 16:56:30 -0300

    Routes and Knex configuration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express';
 
+import routes from './routes';
+
 const app = express();
+app.use(express.json());
 
-app.get('/users', (req, res) => {
-  console.log('Listagem de usuários');
-
-  res.json(['André', 'Diego', 'Lucas', 'Fábio']);
-});
+app.use(routes);
 
 app.listen(3333);
