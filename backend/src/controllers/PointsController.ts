352b532901fd065f908a103af7c8415d97ef6742
andrefangeloni <andrefangeloni@gmail.com>
2020-07-23 00:18:09 -0300

    Added Points Index and CORS

--- backend/src/controllers/PointsController.ts
+++ backend/src/controllers/PointsController.ts
@@ -3,6 +3,24 @@ import { Request, Response } from 'express';
 import knex from '../database/connection';
 
 class PointsController {
+  async index(req: Request, res: Response) {
+    const { city, uf, items } = req.query;
+
+    const parsedItems = String(items)
+      .split(',')
+      .map((item) => Number(item.trim()));
+
+    const points = await knex('points')
+      .join('point_items', 'points.id', '=', 'point_items.point_id')
+      .whereIn('point_items.item_id', parsedItems)
+      .where('city', String(city))
+      .where('uf', String(uf))
+      .distinct()
+      .select('points.*');
+
+    return res.json(points);
+  }
+
   async show(req: Request, res: Response) {
     const { id } = req.params;
 
@@ -32,6 +50,8 @@ class PointsController {
       items,
     } = req.body;
 
+    const trx = await knex.transaction();
+
     const point = {
       image: 'image-fake',
       name,
@@ -43,7 +63,7 @@ class PointsController {
       uf,
     };
 
-    const insertedIds = await knex('points').insert(point);
+    const insertedIds = await trx('points').insert(point);
 
     const point_id = insertedIds[0];
 
@@ -54,7 +74,9 @@ class PointsController {
       };
     });
 
-    await knex('point_items').insert(pointItems);
+    await trx('point_items').insert(pointItems);
+
+    await trx.commit();
 
     return res.status(201).json({ id: point_id, ...point });
   }
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -7,6 +7,7 @@ const routes = express.Router();
 
 routes.get('/items', ItemsController.index);
 
+routes.get('/points', PointsController.index);
 routes.get('/points/:id', PointsController.show);
 routes.post('/points', PointsController.store);
 
