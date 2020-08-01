78dd992b3524a1508e12a8cecbbc1ddc3a050697
andrefangeloni <andrefangeloni@gmail.com>
2020-08-01 11:13:50 -0300

    feat(icons): Added react native vector icons

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { View, Text, Image, ImageBackground } from 'react-native';
 
+import { RectButton } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/Feather';
+
 import styles from './styles';
 
 const Home = () => {
@@ -16,6 +19,15 @@ const Home = () => {
         <Text style={styles.description}>
           Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente
         </Text>
+      </View>
+
+      <View style={styles.footer}>
+        <RectButton style={styles.button}>
+          <View style={styles.buttonIcon}>
+            <Icon name="arrow-right" size={24} color="#fff" />
+          </View>
+          <Text style={styles.buttonText}>Entrar</Text>
+        </RectButton>
       </View>
     </ImageBackground>
   );
