fe9a41e0b4e554ae46b0602af8e40fcd7268f388
andrefangeloni <andrefangeloni@gmail.com>
2020-08-22 22:45:51 -0300

    Setup celebrate

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { celebrate, Joi } from 'celebrate';
 import multer from 'multer';
 
 import multerConfig from './config/multer';
@@ -14,6 +15,24 @@ routes.get('/items', ItemsController.index);
 routes.get('/points', PointsController.index);
 routes.get('/points/:id', PointsController.show);
 
-routes.post('/points', upload.single('image'), PointsController.store);
+routes.post(
+  '/points',
+  upload.single('image'),
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.number().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().max(2),
+      items: Joi.string().required(),
+    }),
+  }, {
+    abortEarly: false,
+  }),
+  PointsController.store,
+);
 
 export default routes;
